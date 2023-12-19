211c690fa50724f89f01f1981e808582913c1d9f
calinjovrea <calinjovrea@gmail.com>
2023-12-19 19:16:22 +0200

    update register backend & frontend
    updates in the register implementations uploaded

--- project_school_dockerized/backend/controllers/register.controller.js
+++ project_school_dockerized/backend/controllers/register.controller.js
@@ -8,47 +8,64 @@ const jwt = require('jsonwebtoken');
 
 const handleNewUser = async (req,res) => {
 
-    if (!req.query.email || !req.query.password &&!req.query.firstName&&!req.query.middleName&&!req.query.lastName&&!req.query.roles) return res.status(400).json({ 'message': 'Email, password, First Name, Middle Name, Last Name are required'});
+    if(req.body){
+        if (!req.body.email && !req.body.password && !req.body.firstName&&!req.body.middleName&& !req.body.lastName) 
+        return res.status(400).json({ 'message': 'Email, password, First Name, Middle Name, Last Name are required'});
+    }else if(req.query){
+        if(!req.query.email && !req.query.password &&!req.query.firstName&&!req.query.middleName&&!req.query.lastName)
+        return res.status(400).json({ 'message': 'Email, password, First Name, Middle Name, Last Name are required'});
+    }else{
+        return res.status(400).json({ 'message': 'Email, password, First Name, Middle Name, Last Name are required'});
+    }
+    
 
     const duplicate = await User.findOne({
         where: {
-            email: req.query.email 
+            email: req.body.email 
         },
     });
-    console.log(duplicate);
+
     if (duplicate) return res.sendStatus(409); //Conflict
     try{
-        const hashedPassword = await bcrypt.hash(req.query.password,10);
-
-        if (!req.query.email) {
-            res.status(400).send({
-                message: "Content can not be empty!"});
-            return;
-        }
+        const hashedPassword = await bcrypt.hash(req.body.password || req.query.password,10);
+        
+        if(req.body){
+            if (!req.body.email) {
+                res.status(400).send({
+                    message: "Content can not be empty!"});
+                return;
+            }
+        }else if(req.query){
+            if (!req.query.email) {
+                res.status(400).send({
+                    message: "Content can not be empty!"});
+                return;
+            }
+        } 
 
         const refreshToken = jwt.sign(
-            {'username': req.query.email},
+            {'username': req.query.email || req.body.email},
             process.env.REFRESH_TOKEN_SECRET,
             {expiresIn: '1d'}
         );
 
         // Create a User
         const user = {
-        firstName: req.query.firstName,
-        middleName: req.query.middleName,
-        lastName: req.query.lastName,
-        email: req.query.email,
+        firstName: req.body.firstName || req.query.firstName,
+        middleName: req.body.middleName || req.query.middleName,
+        lastName: req.body.lastName || req.query.lastName,
+        email: req.body.email || req.query.email,
         password: hashedPassword,
         status: 1,
         refreshToken:refreshToken,
-        roles: req.query.roles
+        roles: ''
         };
     
         // Save User in the database
         User.create(user)
         .then(data => {
             res.send(data);
-            //res.status(200).json({"message": 'Sucessful'});
+            // res.status(200).json({"message": 'Sucessful'});
         })
         .catch(err => {
             res.status(500).send({
@@ -56,7 +73,7 @@ const handleNewUser = async (req,res) => {
                 err.message || "Some error occurred while creating the User."
             });
         });
-        //res.status(201).json({'success': `New User ${email} created!`});
+        res.status(201).json({'success': `New User ${req.body.email || req.query.email} created!`});
 
     }catch (err){
         res.status(500).json({'message': err.message});
