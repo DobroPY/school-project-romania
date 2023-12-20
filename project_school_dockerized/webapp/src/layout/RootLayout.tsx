edb12c9ee2bf108d7071dbf8069ce27bec9d0916
PaulTiplea7843 <paul.tiplea2020@gmail.com>
2023-12-20 12:57:27 +0200

    change auth redirect
    upload auth redirect

--- project_school_dockerized/webapp/src/layout/RootLayout.tsx
+++ project_school_dockerized/webapp/src/layout/RootLayout.tsx
@@ -3,7 +3,9 @@ import { Outlet ,Navigate } from "react-router-dom";
 import AuthContext from "../index";
 const RootLayout = () => {
      const {auth} = useContext(AuthContext);
-    if (!auth) return <Navigate to="/login" replace />
+     console.log(auth)
+    if (auth) {return <Navigate to="/dashboard" replace />}
+ 
      return(
 
   <main className="root-main">
